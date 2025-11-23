09d0b62252bc8c81185be7f57b05c96ddf5bfc0e
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-11-24 04:22:09 +0700

    feat: add fallback currency exchange item

--- src/CurrencyExchange/CurrencyExchange/CurrencyExchangeCommandsProvider.cs
+++ src/CurrencyExchange/CurrencyExchange/CurrencyExchangeCommandsProvider.cs
@@ -2,27 +2,27 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using CurrencyExchange.Pages;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
+using System;
 
 namespace CurrencyExchange;
 
 public partial class CurrencyExchangeCommandsProvider : CommandProvider
 {
-    private readonly ICommandItem[] _commands;
+    private readonly IFallbackCommandItem[] fallbacks = [new FallbackCurrencyExchangeItem()];
 
     public CurrencyExchangeCommandsProvider()
     {
         DisplayName = "Currency Exchange";
         Icon = IconHelpers.FromRelativePath("Assets\\StoreLogo.png");
-        _commands = [
-            new CommandItem(new CurrencyExchangePage()) { Title = DisplayName },
-        ];
     }
 
     public override ICommandItem[] TopLevelCommands()
     {
-        return _commands;
+        return Array.Empty<ICommandItem>();
     }
 
+    public override IFallbackCommandItem[] FallbackCommands() => fallbacks;
 }
---
+++ src/CurrencyExchange/CurrencyExchange/Pages/FallbackCurrencyExchangeItem.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.CommandPalette.Extensions.Toolkit;
+using System;
+using System.Text.RegularExpressions;
+
+namespace CurrencyExchange.Pages;
+
+internal sealed partial class FallbackCurrencyExchangeItem() : FallbackCommandItem(new NoOpCommand(), "Fallback")
+{
+    public override void UpdateQuery(string query)
+    {
+        var match = ExchangeRegex().Match(query.Trim());
+        if (!match.Success
+            || !match.Groups.TryGetValue("amount", out var amountGroup)
+            || !match.Groups.TryGetValue("from", out var fromCurrency))
+        {
+            Title = string.Empty;
+            Subtitle = string.Empty;
+            return;
+        }
+
+        var amountSpan = RemoveCommas(amountGroup.ValueSpan);
+        if (!double.TryParse(amountSpan, out var amount))
+        {
+            Title = string.Empty;
+            Subtitle = string.Empty;
+            return;
+        }
+
+        Title = $"{amount * 2800} USDT";
+        Subtitle = $"{amount} {fromCurrency}";
+    }
+
+    private static ReadOnlySpan<char> RemoveCommas(ReadOnlySpan<char> source)
+    {
+        var commaCount = 0;
+        foreach (var c in source)
+        {
+            if (c == ',')
+            {
+                commaCount++;
+            }
+        }
+
+        if (commaCount == 0)
+        {
+            return source;
+        }
+
+        var dest = new char[source.Length - commaCount];
+        var writeIndex = 0;
+        for (var i = 0; i < source.Length; i++)
+        {
+            var currentChar = source[i];
+            if (currentChar != ',')
+            {
+                dest[writeIndex] = currentChar;
+                writeIndex++;
+            }
+        }
+        return dest;
+    }
+
+    [GeneratedRegex(@"^(?<amount>\d{1,3}(?:,\d{3})*(?:\.\d+)?)\s?(?<from>[a-zA-Z$]+)$")]
+    private static partial Regex ExchangeRegex();
+}
